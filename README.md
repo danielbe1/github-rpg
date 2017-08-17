0daa662002cd98c4fcf9c0a626d487cf2c716042
Daniel Beck <daniel.beck@supersonic.com>
2017-08-18 02:19:49 +0300

    take into account ignore label

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # github-rpg
+
+
+Issues <--> Tasks in a nutshell
+==============================
+* When opening a new Github issue, a new `todo` task will be created on your Habitica account.
+* When closing an existing Github issue, the corresponding Habitica `todo` task will be completed, if the task does not exist it will be recreated.
+* When reopening a closed Github issue, the corresponding Habitica `todo` task will be uncompleted, if it does not exist it will be recreated.
+
+Task hints in Github issues
+===========================
+It is possible to specify certain hints on the Github issue to specify task parameters:
+* Difficulty: 
+    - `Difficulty: trivial`
+    - `Difficulty: easy`
+    - `Difficulty: medium`
+    - `Difficulty: hard`
+
+Excluding Github issues
+=======================
+Specific issues can be excluded from being synced by adding a label, defined in the `LABEL_TO_IGNORE` env variable (set to `github-rpg-ignore` by default).
+Adding or removing this label from existing issues will acuse the corresponding `todo` tasks to be deleted or recreated accordingly.
--- github/github_api.py
+++ github/github_api.py
@@ -22,15 +22,13 @@ class GithubWebhookHandler:
         if 'action' not in request_json:
             raise GithubWebhookMalformedEventError('issues', 'missing "action" field')
 
-        logging.info(f'handling issues event')
-
         action = request_json['action'].lower()
         issue = GithubIssueObject.from_json(request_json)
-
-        if issue.label_assigned(self.ignored_label) and issue == 'labeled':
-            logging.info('ignore label is set, task will be deleted if required')
-
+        logging.info(request_json)
+        if issue.label_assigned(self.ignored_label) and action == 'labeled':
             self.delete_task_if_exists(issue)
+        elif not issue.label_assigned(self.ignored_label) and action == 'unlabeled':
+            self.create_task_if_does_not_exists(issue)
         elif action == 'opened':
             self.handle_issue_opened(issue)
         elif action == 'reopened':
@@ -43,10 +41,20 @@ class GithubWebhookHandler:
             logging.info(f'ignoring unhandled issues action {action}')
 
     def delete_task_if_exists(self, issue):
+        logging.info('ignore label is set, task will be deleted if required')
+
         task = self.habitica_api_client.get_todo_task(issue.alias)
 
         if task is not None:
             self.habitica_api_client.delete_todo_task(issue.alias)
+
+    def create_task_if_does_not_exists(self, issue):
+        logging.info('ignore label is not set, task will be created if required')
+
+        task = self.habitica_api_client.get_todo_task(issue.alias)
+
+        if task is None:
+            self.habitica_api_client.create_new_todo_task(issue.alias, issue.title, issue.habitica_task_difficulty)
 
     def handle_issue_opened(self, issue):
         logging.info(f'issue {issue.number} titled "{issue.title}" opened')
